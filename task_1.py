3bb211a3a2afb34fc621712bbb45879e03db90f0
kacper651 <kacper.wroblewski651@gmail.com>
2022-10-30 11:08:15 +0100

    finished task 1

--- task_1.py
+++ task_1.py
@@ -1,2 +1,23 @@
 def remove_parentheses(text):
+    i1 = ""
+    i2 = ""
+
+    if ('(' or ')') not in text:
+        print("Nie ma co usuwać ¯\_(ツ)_/¯")
+        return text
+
+    for character in text:
+        if character == '(':
+            i1 = text.index(character)
+        if character == ')':
+            i2 = text.index(character)
+        if i1 != "" and i2 != "":
+            text = text[0:i1] + text[i2+2:len(text)]
+            i1 = ""
+            i2 = ""
     return text
+
+
+string = "(Nie) jest (tak) źle"
+
+print(remove_parentheses(string))
