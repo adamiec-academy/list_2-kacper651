1c4569fbc9590d4e8972b2f2a611b808c04deeb9
kacper651 <kacper.wroblewski651@gmail.com>
2022-10-31 18:54:15 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,32 +1,38 @@
 def cipher(text, shift):
     result = ""
     for char in text:
-        unicode_char = ord(char)
-        if unicode_char + shift < 122 and not char.isupper():
-            unicode_char += shift
-        elif unicode_char + shift < 90:
-            unicode_char += shift
+        if char != " ":
+            unicode_char = ord(char)
+            if unicode_char + shift <= 122 and not char.isupper():
+                unicode_char += shift
+            elif unicode_char + shift <= 90 and char.isupper():
+                unicode_char += shift
+            else:
+                unicode_char = unicode_char - 26 + shift
+            char = chr(unicode_char)  # za wiedzialem jak zmieniac pojedyncze znaki w stringu, ale tak tez dziala xd
+            result += char
         else:
-            unicode_char -= 26 + shift
-        char = chr(unicode_char)  # za wiedzialem jak zmieniac pojedyncze znaki w stringu, ale tak tez dziala xd
-        result += char
+            result += char
     return result
 
 
 def decipher(text, shift):
     result = ""
     for char in text:
-        unicode_char = ord(char)
-        if unicode_char - shift > 97 and not char.isupper():
-            unicode_char -= shift
-        elif unicode_char - shift > 65:
-            unicode_char -= shift
+        if char != " ":
+            unicode_char = ord(char)
+            if unicode_char - shift >= 97 and not char.isupper():
+                unicode_char -= shift
+            elif unicode_char - shift >= 65 and char.isupper():
+                unicode_char -= shift
+            else:
+                unicode_char = unicode_char + 26 - shift
+            char = chr(unicode_char)
+            result += char
         else:
-            unicode_char += 26 - shift
-        char = chr(unicode_char)
-        result += char
+            result += char
     return result
 
 
-print(cipher("Imperator", 1))
-print(decipher("Jnqfsbups", 1))
+print(cipher("Alfa i Omega", 13))
+print(decipher("Nysn v Bzrtn", 13))
